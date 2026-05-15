ff243f3b3d513f302ddc954f518fda763e766c05
unknown <you@example.com>
2026-05-15 12:34:27 +0530

    fixed backend crash

--- server/server.js
+++ server/server.js
@@ -3,23 +3,34 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:3000"
-}));
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
-
-app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server started");
+app.get("/", (req, res) => {
+  res.send("TaskFlow Backend Running");
 });
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+
+    console.log("MongoDB Connected");
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  })
+  .catch((error) => {
+    console.log(error);
+  });
